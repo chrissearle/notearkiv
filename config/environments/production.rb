381fc3839fbc9c86d1884ce23e6e9c1862801d7e
Chris Searle <chris@chrissearle.org>
2012-01-23 13:45:18 +0100

    Fix precompile list

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,8 @@ Notearkiv2::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
+  config.assets.precompile += %w( grid.css ie.css mobile.css small.css )
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
