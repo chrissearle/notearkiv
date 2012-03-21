19e2e71f3b2298a57236336e61c42abc33738fa5
Chris Searle <chris@chrissearle.org>
2012-03-21 15:45:29 +0100

    Add themed genre scaffold as example

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Notearkiv::Application.routes.draw do
+  resources :genres
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Genre.create([
+  { id: 1,  name: "Chant" },
+  { id: 2,  name: "Epos" },
+  { id: 3,  name: "Folketone" },
+  { id: 4,  name: "Hymne" },
+  { id: 5,  name: "Irsk folketone" },
+  { id: 6,  name: "Jul" },
+  { id: 7,  name: "Kantate" },
+  { id: 8,  name: "Koral" },
+  { id: 9,  name: "Liturgi" },
+  { id: 10, name: "Madrigal" },
+  { id: 11, name: "Mag & Nunc" },
+  { id: 12, name: "Motett" },
+  { id: 13, name: "Nasjonalsang" },
+  { id: 14, name: "Oratorium" },
+  { id: 15, name: "Pop" },
+  { id: 16, name: "Preces & Responses" },
+  { id: 17, name: "Salme" },
+  { id: 18, name: "Spiritual" },
+  { id: 19, name: "Te Deum" },
+  { id: 20, name: "Verdslig" },
+  { id: 21, name: "Vise" },
+  { id: 22, name: "Kirkelig" },
+  { id: 23, name: "Anthem" },
+  { id: 24, name: "Samtid" }
+])
