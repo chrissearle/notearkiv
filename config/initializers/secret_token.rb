5de2c2b048584da1c762c6df074cbf39e1e7691d
Chris Searle <chris@chrissearle.org>
2012-03-21 14:45:43 +0100

    Blank rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notearkiv::Application.config.secret_token = 'd0ab333ae460e52fcfb50767eccfd8ddecebfbcdd972addeec5022430a06c130422153be99425064d981fd397e8e6dd6cb68545b73ebd8b82d16260cb6ba2e2a'
