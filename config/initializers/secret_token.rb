4a4c91e6e11b3b4ccab60c1606df11f052088edf
Chris Searle <chris@chrissearle.org>
2012-03-21 14:42:08 +0100

    Clear down

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Notearkiv2::Application.config.secret_token = '9df06f32c2854fcfa2805a9be14190cae3e2fcbbf59f38076e530372ea96629f2304e201c044e0503406bcfcaf66875af8a1df4e8cb89382aff6273bce5f5365'
