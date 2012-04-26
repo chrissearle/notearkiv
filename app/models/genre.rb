b02401e7f862cdd9a8b3addb3c959b64256b1271
Chris Searle <chris@chrissearle.org>
2012-04-26 16:49:07 +0200

    Update genre for evensong

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,6 @@
 class Genre < ActiveRecord::Base
 #  has_many :notes
-#  has_many :evensongs
+  has_many :evensongs
 
   validates_presence_of :name
 
@@ -8,8 +8,8 @@ class Genre < ActiveRecord::Base
 #  scope :preloaded, :include => [:notes, :evensongs]
 
   def deletable?
-#    notes.size() == 0 && evensongs.size() == 0
-    true
+#    notes.size() == 0 &&
+    evensongs.size() == 0
   end
 
 end
