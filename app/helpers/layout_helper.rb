a17a478ccdb8abdd4d5e7c5e9fe8cbb8cdfe1ab4
Chris Searle <chris@chrissearle.org>
2012-04-30 12:50:02 +0200

    Move upload link to helper

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -34,7 +34,7 @@ module LayoutHelper
   end
 
   def danger_button_class(object)
-    if (object.deletable?)
+    if object.deletable?
       "btn btn-mini btn-danger"
     else
       "btn btn-mini btn-danger disabled"
@@ -47,5 +47,28 @@ module LayoutHelper
     end
 
     link_to value, search_path(:search => value)
+  end
+
+  def media_link(upload)
+    unless upload.media
+      return upload.display_name
+    end
+
+    css_class = "icon-file"
+
+    if upload.path.ends_with? "pdf"
+      css_class = "icon-book"
+    end
+    if upload.path.ends_with? "zip"
+      css_class = "icon-download-alt"
+    end
+    if upload.path.ends_with? "mp3"
+      css_class = "icon-music"
+    end
+    if upload.path.ends_with? "m4a"
+      css_class = "icon-music"
+    end
+
+    link_to "<i class='#{css_class}'></i>".html_safe + upload.display_name, upload.media
   end
 end
