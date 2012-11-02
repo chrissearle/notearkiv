8b9183afcb5c1984827a576e18250184a3c06e7f
Chris Searle <chris@chrissearle.org>
2012-11-02 15:38:21 +0100

    Strip accents for prefix (autocomplete) search

---
+++ app/models/abstract_note.rb
@@ -0,0 +1,19 @@
+module AbstractNote
+  def remove_links
+    self.links.each do |link|
+      link.destroy
+    end
+  end
+
+  def remove_uploads
+    self.uploads.each do |upload|
+      upload.destroy
+    end
+  end
+
+  def typeahead(prefix)
+    get_typeahead.map do |name|
+      name and name.parameterize.split((/\W+/))
+    end.flatten.select { |candidate| candidate and candidate.start_with? prefix.parameterize }.uniq
+  end
+end
