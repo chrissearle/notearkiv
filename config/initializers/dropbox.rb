3c888adba89cf942e8a377669f933fa7dcf01523
Chris Searle <chris@chrissearle.org>
2013-01-03 13:45:10 +0100

    Dalli tries to change encoding on cache keys - can't freeze them any more

--- config/initializers/dropbox.rb
+++ config/initializers/dropbox.rb
@@ -1,9 +1,9 @@
 require 'dropbox_sdk'
 
-DROPBOX_SESSION_CACHE_KEY = "dropbox.session".freeze
-DROPBOX_FILE_LIST_CACHE_KEY = "dropbox.file.list".freeze
-DROPBOX_FILE_LIST_TIMESTAMP_CACHE_KEY = "dropbox.file.timestamp".freeze
-DROPBOX_FILE_MEDIA_PREFIX_CACHE_KEY = "dropbox.media.".freeze
+DROPBOX_SESSION_CACHE_KEY = "dropbox.session"
+DROPBOX_FILE_LIST_CACHE_KEY = "dropbox.file.list"
+DROPBOX_FILE_LIST_TIMESTAMP_CACHE_KEY = "dropbox.file.timestamp"
+DROPBOX_FILE_MEDIA_PREFIX_CACHE_KEY = "dropbox.media."
 
 DROPBOX_FILE_ICONS = {
     :pdf => 'icon-book',
