fae78cf9b0dad79afcdfb9d1c588848a98facb8a
Chris Searle <chris@chrissearle.org>
2013-01-24 11:51:44 +0100

    Add language

--- app/models/note.rb
+++ app/models/note.rb
@@ -8,6 +8,7 @@ class Note < ActiveRecord::Base
   belongs_to :composer
   belongs_to :genre
   belongs_to :period
+  belongs_to :language
 
   has_many :note_language_assignments
   has_many :languages, :through => :note_language_assignments
