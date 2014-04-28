07e908860ab891feb4fdb865fd53c8bf5f51fcc8
chrsearl <chris.searle@finn.no>
2014-04-28 13:28:47 +0200

    ÆØÅ in search

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
       return ''
     end
 
-    link_to link_with_icon('search', value), search_path(:search => value.to_s.gsub(/[^\wåøæÅØÆ ]/, '').split.first)
+    link_to link_with_icon('search', value), search_path(:search => value.to_s.gsub(/[^\p{Word}\s]/, '').split.trim.first)
   end
 
 
