f1dcc1daf40b79c1f8b5f9f4f2afa25434ff1a20
Chris Searle <chris@chrissearle.org>
2015-02-27 11:23:15 +0100

    Test change for edit link

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.delivery_method = :smtp
 
 ActionMailer::Base.smtp_settings = {
     :address  => ENV['MAIL_SERVER'],
-    :port  => 25,
+    :port  => 587,
     :user_name  => ENV['MAIL_ACCOUNT'],
     :password  => ENV['MAIL_PASSWORD'],
     :domain => ENV['MAIL_DOMAIN'],
